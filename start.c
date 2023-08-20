1d19fdc40de965231c44d70e392365bad1c31991
Armybed22 <safgatash@mail.ru>
2023-08-20 16:28:50 +0500

    bugfix: 0.4.5

--- start.c
+++ start.c
@@ -23,19 +23,41 @@ char* concat(char *s1, char *s2)
 
 int main(int argc, char *argv[])
 {
-	printf("%d",argc);
-	if (argc == 1)
-	{
-		system("python3 aplfh.py");
-		return 0;
-	}
+    printf("%d",argc);
 
-	if (argc > 2)
-	{
-		printf("Error, too many arguments");
-		return 0;
-	}
-	char *s = concat("python3 aplfh.py ", argv[1]);
-	system(s);
-	return 0;
+    char* ostype = getenv("OSTYPE");
+    if (ostype == NULL)
+    {
+       if (argc == 1)
+               {
+                   system("python aplfh.py");
+                   return 0;
+               }
+       
+               if (argc > 2)
+               {
+                   printf("Error, too many arguments");
+                   return 0;
+               }
+               char *s = concat("python aplfh.py ", argv[1]);
+               system(s);
+    }
+    else
+    {         
+        if (argc == 1)
+        {
+            system("python3 aplfh.py");
+            return 0;
+        }
+
+        if (argc > 2)
+        {
+            printf("Error, too many arguments");
+            return 0;
+        }
+        char *s = concat("python3 aplfh.py ", argv[1]);
+        system(s);
+    }
+    
+    return 0;
 }
